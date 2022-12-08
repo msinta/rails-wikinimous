6b86c49d3fa5f314459817aa6b0a77f9501acda6
mohamed shegow <shegowmo@gmail.com>
2022-12-08 18:23:07 +0900

    completed more routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
-
-
 10.times do
   Article.create(title: Faker::Company.bs, content: Faker::Books::Dune.quote )
 end
